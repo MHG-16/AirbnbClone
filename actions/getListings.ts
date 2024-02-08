819447fb0983a4be7faab1bc6688a648d956b196
MHG1816 <hammaguibeneess@gmail.com>
2024-02-08 09:58:04 +0100

    get listing by params

--- actions/getListings.ts
+++ actions/getListings.ts
@@ -40,3 +40,89 @@ export async function getListingsByUserId(userId: string) {
         throw new Error(err);
     }
 }
+
+export interface IListingParams {
+    guestCount?: number;
+    roomCount?: number;
+    bathroomCount?: number;
+    startDate?: string;
+    endDate?: string;
+    locationValue?: string;
+    category?: string;
+}
+
+export async function    getListingsByparams(params: IListingParams) {
+    try {
+        const {
+            roomCount,
+            guestCount,
+            bathroomCount,
+            locationValue,
+            startDate,
+            endDate,
+            category,
+        } = params;
+
+        let query: any = {};
+
+        if (category) {
+            query.category = category
+        };
+
+        if (roomCount) {
+            query.roomCount = {
+                gte: +roomCount
+            }
+        }
+
+        if (bathroomCount) {
+            query.bathroomCount = {
+                gte: +bathroomCount
+            }
+        }
+        if (guestCount) {
+            query.guestCount = {
+                gte: +guestCount
+            }
+        }
+
+        if (locationValue) {
+            query.locationValue = locationValue
+        }
+
+        if (startDate && endDate) {
+            query.NOT = {
+                reservation: {
+                    some: {
+                        OR: [
+                            {
+                                endDate: {gte: startDate},
+                                startDate: {lte: startDate}
+                            },
+                            {
+                                startDate: {lte: endDate},
+                                endDate: {gte: endDate}
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+
+        const listings = await db.listing.findMany({
+            where: query,
+            orderBy: {
+                createdAt: "desc"
+            }
+        });
+
+        const safeListings = listings.map((listing) => ({
+            ...listing,
+            createdAt: listing.createdAt.toISOString(),
+        }));
+
+        return safeListings;
+    } catch (error: any) {
+        throw new Error(error)
+    }
+}
